8b2bd0411853169dffff7d7bcdf00e7e609e886f
Alex Polvi <alex@polvi.net>
2025-01-19 18:11:47 -0800

    add user widget

---
+++ sentry.js
@@ -0,0 +1,16 @@
+window.sentryOnLoad = function() {
+  Sentry.init({
+    // add other configuration here
+  });
+
+  Sentry.lazyLoadIntegration("feedbackIntegration")
+    .then((feedbackIntegration) => {
+      Sentry.addIntegration(feedbackIntegration({
+        // User Feedback configuration options
+      }));
+    })
+    .catch(() => {
+      // this can happen if e.g. a network error occurs,
+      // in this case User Feedback will not be enabled
+    });
+};
