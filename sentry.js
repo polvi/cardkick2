b8cd01c9e060f7c6955efe80f22ac7caffeefa5b
Alex Polvi <alex@polvi.net>
2025-01-19 18:17:00 -0800

    inject

--- sentry.js
+++ sentry.js
@@ -6,6 +6,7 @@ window.sentryOnLoad = function() {
   Sentry.lazyLoadIntegration("feedbackIntegration")
     .then((feedbackIntegration) => {
       Sentry.addIntegration(feedbackIntegration({
+  	autoInject: true
         // User Feedback configuration options
       }));
     })
